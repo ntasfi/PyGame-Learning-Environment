04578dec923f79c65bbb2172a5c73c052667a8df
norm <ntasfi@gmail.com>
2016-05-08 21:02:09 -0400

    fixes, added more scenarios

--- ple/games/tilebase/scenarios/blockeddoor.py
+++ ple/games/tilebase/scenarios/blockeddoor.py
@@ -3,10 +3,10 @@ import numpy as np
 from . import base
 from .. import tiles
 
-class LightKey(base.Scenario):
+class BlockedDoor(base.Scenario):
 
     def __init__(self, rng):
-        block_strings = ["GOA", "SSW_1", "AG", "WA", "BU"] #blocks we use
+        block_strings = ["GOA", "BM", "AG", "WA", "BU"] #blocks we use
         base.Scenario.__init__(self, rng, block_strings)
 
     def generate(self):
@@ -16,12 +16,13 @@ class LightKey(base.Scenario):
         self.map_str[ locs ] = self.EMPTY_TILE
         
         #select a column as the wall
-        col = self.rng.choice(np.arange(w/2 - 1, w/2 + 1))
+        col = self.rng.choice(np.arange(w/2, w/2 + 1))
         self.map_str[:, col] = "BU"
 
         #pick a location for the door
         door = self.rng.randint(0, high=h-1)
-        self.map_str[door, col] = "DO_1"
+        self.map_str[door, col] = "BM"
+        self.door_pos = [door, col]
 
         ranges = [(0, col), (col+1, w-1)]
         side = self.rng.choice([0, 1])
@@ -35,20 +36,17 @@ class LightKey(base.Scenario):
         #random side for agent
         self.map_str[pos_h, pos_w] = "AG"
 
-        while self.map_str[ pos_h, pos_w ] != self.EMPTY_TILE:
-            pos_w = self.rng.randint(side[0], high=side[1])
-            pos_h = self.rng.randint(0, high=h-1)
-
-        #put switch here too
-        self.map_str[pos_h, pos_w] = "SSW_1"
-
         #random side for goal
         side = self.rng.choice([0, 1])
         side = ranges[side]
-        while self.map_str[ pos_h, pos_w ] != self.EMPTY_TILE:
+        tries = 0
+        while self.map_str[ pos_h, pos_w ] != self.EMPTY_TILE or self._blocking(pos_w, pos_h):
             pos_w = self.rng.randint(side[0], high=side[1])
             pos_h = self.rng.randint(0, high=h-1)
-        
+            tries += 1 
+            if tries > 30:
+                break
+            
         self.map_str[pos_h, pos_w] = "GOA"
 
         #make 20% of remaining blocks randomly water or unmoveable blocks
@@ -83,8 +81,12 @@ class LightKey(base.Scenario):
         h = np.clip(pos[:, 0], 0, self.map_str.shape[0]-1) #the heights
         w = np.clip(pos[:, 1], 0, self.map_str.shape[1]-1)
         area = self.map_str[(h, w)].tolist()
-       
-        if "DO_1" in area:
+     
+        w_dist = np.absolute(self.door_pos[1]-pos_w)
+        if w_dist < 3 and pos_h == self.door_pos[0]: #same level AND its close enough to block 
+            return True
+
+        if "BM" in area:
             return True #its blocking a door
 
         if "BU" in area: #we dont want too many blocks near e/o
